b7398fa3c2c807f16d51b50c81898847fd43df72
unknown <ryandraarif@gmail.com>
2025-06-04 23:07:28 +0700

    profile-ryan

--- routes/lectures.js
+++ routes/lectures.js
@@ -4,4 +4,8 @@ const LectureController = require('../controllers/lecture');
 
 // router.get('/', );
 
+router.get('/lectures', LectureController.listLectures);
+router.get('/lectures/:lectureId', LectureController.showLecture);
+router.get('/lectures/:lectureId/buy', LectureController.buyLecture);
+
 module.exports = router;
