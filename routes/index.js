5f1f5dc0812e2d5addd6377c5322847f78a19b61
WahyuSattriana <wahyusattriana@gmail.com>
2025-06-04 19:56:12 +0800

    feat: add login, register, logout

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,24 @@ const lectureRoutes = require('./lectures');
 const profileRoutes = require('./profile');
 const myLectureRoutes = require('./myLectures');
 
+const authMiddleware = (req, res, next) => {
+  if (req.session.userId) {
+    next();
+  } else {
+    const error = 'Login dulu brok!';
+    res.redirect(`/login?error=${error}`);
+  }
+}
+
 router.get('/', Main.homepage);
+router.get('/login', Main.renderLogin);
+router.post('/login', Main.handlerLogin);
+router.get('/register', Main.renderRegister);
+router.post('/register', Main.handlerRegister);
+router.get('/logout', Main.handlerLogout);
 
 router.use('/lectures', lectureRoutes);
-router.use('/profile', profileRoutes);
-router.use('/myLecture', myLectureRoutes);
+router.use('/profile', authMiddleware, profileRoutes);
+router.use('/myLecture', authMiddleware, myLectureRoutes);
 
 module.exports = router;
