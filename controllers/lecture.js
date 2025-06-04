6bfaa58b1aaf349caa5ed93251d7803da9685ee1
unknown <ryandraarif@gmail.com>
2025-06-04 17:05:52 +0700

    'profile-ryan'

--- controllers/lecture.js
+++ controllers/lecture.js
@@ -1,5 +1,21 @@
-class Lecture {
+const { Lecture, Challenge, UserLecture, User } = require('../models/index');
 
+class Lecture {
+    static async listLectures(req, res) {
+        try {
+            let lectures = await Lecture.findAll({
+                include: {
+                    model: Challenge,
+                    attributes: ['title']
+                }
+            });
+            res.render('lectures/index', { lectures });
+        } catch (err) {
+            res.send(err);
+        }
+    }
+
+    
 }
 
 module.exports = Lecture;
